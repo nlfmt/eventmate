0aa1a802b91128db49a8784b613fd799a8f2c9eb
Despoina Sfantou <ds215@hdm-stuttgart.de>
2023-06-14 13:35:23 +0200

    add edit password and account-info

--- src/components/forms/ChangeAccountInfo.tsx
+++ src/components/forms/ChangeAccountInfo.tsx
@@ -1,37 +1,151 @@
 import React from "react";
 
-import c from "./ChangePassword.module.scss";
+import c from "./ChangeAccountInfo.module.scss";
+import common from "@/styles/common.module.scss";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { api } from "@/utils/api";
+import { useSession } from "next-auth/react";
+import { CheckRounded, EditRounded } from "@mui/icons-material";
 
 type ChangeAccountInfoSchema = {
   username: string;
   email: string;
   bio: string;
-}
+};
 
-const ChangeAccountInfo = () => {
+const ChangeAccountInfoForm = () => {
+  const { data: sessionData } = useSession();
 
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
   } = useForm<ChangeAccountInfoSchema>();
 
-  const { mutateAsync: ChangeAccountInfo } = api.user.changeAccountInfo.useMutation();
+  const [username, setUsername] = React.useState(sessionData?.user.name ?? "");
+  const [email, setEmail] = React.useState(sessionData?.user.email ?? "");
+  const [bio, setBio] = React.useState(sessionData?.user.bio ?? "");
+  const [isEditingUsername, setIsEditingUsername] = React.useState(false);
+  const [displayedUsername, setDisplayedUsername] = React.useState(sessionData?.user.name ?? "");
+  const [isEditingEmail, setIsEditingEmail] = React.useState(false);
+  const [displayedEmail, setDisplayedEmail] = React.useState(sessionData?.user.email ?? "");
+  const [isEditingBio, setIsEditingBio] = React.useState(false);
+  const [displayedBio, setDisplayedBio] = React.useState(sessionData?.user.bio ?? "");
 
-  const submitHandler: SubmitHandler<ChangeAccountInfoSchema> = async (data) => {
+  const { mutateAsync: ChangeAccountInfo } =
+    api.user.changeAccountInfo.useMutation();
+
+  const submitHandler: SubmitHandler<ChangeAccountInfoSchema> = async (
+    data
+  ) => {
     await ChangeAccountInfo(data);
-  }
+    setDisplayedEmail(data.email);
+  };
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleBioChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBio(e.target.value);
+  };
+
+  const handleEditUsername = () => {
+    setIsEditingUsername(true);
+  };
+
+  const handleSaveUsername = () => {
+    setIsEditingUsername(false);
+  };
+
+  const handleEditEmail = () => {
+    setIsEditingEmail(true);
+  };
+
+  const handleSaveEmail = () => {
+    setIsEditingEmail(false);
+    // setEmail(email);
+  };
+
+  const handleEditBio = () => {
+    setIsEditingBio(true);
+  };
+
+  const handleSaveBio = () => {
+    setIsEditingBio(false);
+  };
 
   return (
     <form onSubmit={handleSubmit(submitHandler)} className={c.form}>
-      <input type="text" {...register("username", { required: true })} data-error={!!errors.username} />
-      <input type="text" {...register("email", { required: true })} data-error={!!errors.email} />
-      <input type="text" {...register("bio", { required: true })} data-error={!!errors.bio} />
-      <button type="submit">Submit</button>
+        <div className={c.inputs}>
+        <div className={c.txt_field}>
+          {isEditingUsername ? (
+            <>
+            <input
+              type="text"
+              {...register("username", { required: true })}
+              value={username}
+              onChange={handleUsernameChange}
+              data-error={!!errors.username}
+              placeholder={`${sessionData?.user.name}`}
+            />
+            <div className={c.edit} onClick={handleSaveUsername}>
+              <CheckRounded />
+            </div>
+            </>
+          ) : (
+            <>
+              <label className={c.label}>{displayedUsername}{sessionData?.user.name}</label>
+              <div className={c.edit} onClick={handleEditUsername}>
+                <EditRounded />
+              </div>
+            </>
+          )}
+          </div>
+          <div className={c.txt_field}>
+          {isEditingEmail ? (
+            <>
+              <input
+                type="text"
+                {...register("email", { required: true })}
+                value={email}
+                onChange={handleEmailChange}
+                data-error={!!errors.email}
+                placeholder={`${sessionData?.user.email}`}
+              />
+              <div className={c.edit} onClick={handleSaveEmail}>
+                <CheckRounded />
+              </div>
+            </>
+          ) : (
+            <>
+              <label className={c.label}>{displayedEmail}{sessionData?.user.email}</label>
+              <div className={c.edit} onClick={handleEditEmail}>
+                <EditRounded />
+              </div>
+            </>
+          )}
+        </div>
+        <div className={c.txt_field}>
+          <input
+            type="text"
+            {...register("bio", { required: true })}
+            value={bio}
+            onChange={handleBioChange}
+            data-error={!!errors.bio}
+          />
+          <label className={c.label}>My bio <br />{bio}</label>
+        </div>
+        <button type="submit" className={common.submitButton}>
+          Submit
+        </button>
+      </div>
     </form>
-  )
+  );
 };
 
-export default ChangeAccountInfo;
+export default ChangeAccountInfoForm;
--- src/components/forms/ChangePassword.tsx
+++ src/components/forms/ChangePassword.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-import c from "./ChangePassword.module.scss";
+import c from "./SignupForm.module.scss";
+import common from "@/styles/common.module.scss";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { api } from "@/utils/api";
 import { KeyRounded } from "@mui/icons-material";
@@ -9,42 +10,92 @@ type ChangeAccountInfoSchema = {
   oldPassword: string;
   newPassword: string;
   confirmPassword: string;
-}
+};
 
 const ChangePassword = () => {
+  const [passwordError, setPasswordError] = useState("");
 
   const {
     register,
     handleSubmit,
-    formState: { errors }
+    formState: { errors },
+    getValues,
   } = useForm<ChangeAccountInfoSchema>();
 
   const { mutateAsync: changePassword } = api.user.changePassword.useMutation();
 
-  const submitHandler: SubmitHandler<ChangeAccountInfoSchema> = async (data) => {
-    await changePassword(data);
-  }
+  const submitHandler: SubmitHandler<ChangeAccountInfoSchema> = async (
+    data
+  ) => {
+    // await changePassword(data);
+    setPasswordError("");
+
+    if (data.newPassword !== data.confirmPassword) {
+      setPasswordError("New password and confirm password do not match");
+      return;
+    }
+
+    try {
+      const isOldPasswordCorrect = await api.user.checkPassword(
+        data.oldPassword
+      );
+
+      if (!isOldPasswordCorrect) {
+        setPasswordError("Old password is incorrect");
+        return;
+      }
+
+      await api.user.changePassword(data);
+    } catch (err) {
+      setPasswordError("An error occurred while changing your password");
+    }
+  };
+
+  const validateOldPassword = async (oldPassword: string) => {
+    // const isOldPasswordCorrect = await api.user.checkPassword(oldPassword);
+    // if (isOldPasswordCorrect) {
+    //   return true;
+    // } else {
+    //   return false;
+  };
 
   return (
     <form onSubmit={handleSubmit(submitHandler)} className={c.form}>
-      <div className={c.txt_field}>
-      <KeyRounded />
-      <input type="text" {...register("oldPassword", { required: true })} data-error={!!errors.oldPassword} />
-      <label className={c.label}>Old Password</label>
+      <div className={c.inputs}>
+        <div className={c.txt_field}>
+          <KeyRounded />
+          <input
+            type="password"
+            {...register("oldPassword", { required: true })}
+            data-error={!!errors.oldPassword}
+          />
+          <label className={c.label}>Old Password</label>
+        </div>
+        <div className={c.txt_field}>
+          <KeyRounded />
+          <input
+            type="password"
+            {...register("newPassword", { required: true })}
+            data-error={!!errors.newPassword}
+          />
+          <label className={c.label}>New Password</label>
+        </div>
+        <div className={c.txt_field}>
+          <KeyRounded />
+          <input
+            type="password"
+            {...register("confirmPassword", { required: true })}
+            data-error={!!errors.confirmPassword}
+          />
+          <label className={c.label}>Confirm Password</label>
+        </div>
+        {passwordError && <p className={c.error}>{passwordError}</p>}
+        <button type="submit" className={common.submitButton}>
+          Submit
+        </button>
       </div>
-      <div className={c.txt_field}>
-      <KeyRounded />
-      <input type="text" {...register("newPassword", { required: true })} data-error={!!errors.newPassword} />
-      <label className={c.label}>New Password</label>
-      </div>
-      <div className={c.txt_field} >
-        <KeyRounded />
-      <input type="text" {...register("confirmPassword", { required: true })} data-error={!!errors.confirmPassword} />
-      <label className={c.label}>Confirm Password</label>
-      </div>
-      <button type="submit">Submit</button>
     </form>
-  )
+  );
 };
 
 export default ChangePassword;
