f929ae888e012684d2cb8a8e4cfd7e77993638c2
nlfmt <nlfmt@gmx.de>
2023-06-27 15:54:15 +0200

    add ignore step script

---
+++ ignore-step.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
