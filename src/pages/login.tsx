19d12d1f7b3a1732017a937f7a3528d9cfbdce5d
nlfmt <nlfmt@gmx.de>
2023-07-19 03:58:43 +0200

    remove example code from login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,15 +2,10 @@ import LoginForm from "@/components/forms/LoginForm";
 import { type NextPage } from "next";
 import Head from "next/head";
 
-import common from "../styles/common.module.scss"
 import c from "../components/forms/SignupForm.module.scss"
 import PageWithSidebar from "@/components/PageWithSidebar/PageWithSidebar";
-import LocationSelectDialog, { Location } from "@/components/LocationSelectDialog/LocationSelectDialog";
-import { useState } from "react";
 
 const LoginPage: NextPage = () => {
-
-  const [location, setLocation] = useState<Location | null>(null);
 
   return (
     <>
@@ -25,7 +20,6 @@ const LoginPage: NextPage = () => {
             <br /> - unforgettable experience!
           </p>
           <LoginForm />
-          <LocationSelectDialog selected={location} setSelected={setLocation} />
         </div>
       </PageWithSidebar>
     </>
